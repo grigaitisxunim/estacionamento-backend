3833bc96de83c2838b5e6689c335df5b3167bfab
Anderson Junior <junior.grigaitis98@hotmail.com>
2022-09-20 18:25:29 -0300

    limpeza codigo

--- server.js
+++ server.js
@@ -1,29 +1,12 @@
-require('./src/database')
+require("./src/database");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-const mongoose = require("mongoose");
 const routes = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-mongoose.connect(
-  "mongodb://localhost/omni",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  },
-  function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("MongoDb CONECTADO com sucesso!");
-    }
-  }
-);
 
 app.use(cors());
 app.use(cookieParser());
