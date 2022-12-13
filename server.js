a862b3634763e7d9bc37633523e655dde3bcf345
Leonardo Franco <leonardo.franco@montdata.com>
2022-12-13 14:25:15 +0000

    remove CORS

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require("./src/database");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+/*const cors = require("cors");*/
 const path = require("path");
 const routes = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+/*app.use(cors());*/
 app.use(cookieParser());
 app.use(express.json());
 app.get("/", function (req, res) {
