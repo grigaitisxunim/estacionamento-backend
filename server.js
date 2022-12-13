e754240d54832d15f0b568910ddf792b69d6b13c
Leonardo Franco <leonardo.franco@montdata.com>
2022-12-13 15:17:42 +0000

    Revert "Update server.js"
    This reverts commit def2c02a7b693af667f407da6ee8138bf5b7d3d9

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 require("./src/database");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+/*const cors = require("cors");*/
 const path = require("path");
 const routes = require("./src/routes");
-const whitelist = ["*"]
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors(corsOptions))
+/*app.use(cors());*/
 app.use(cookieParser());
 app.use(express.json());
 app.get("/", function (req, res) {
